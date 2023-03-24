63ec2607268764c6f24816f2e2ca9e8ac4d3b318
Your Name <you@example.com>
2023-03-24 22:44:34 +0100

    Profil Page also ready key is not working

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="profile">
+<html lang="en">
+    <!-- <div class="profile">
       <h4>City List</h4>
       <br>
       <div class="row">
@@ -19,7 +20,6 @@
               <p class="Email">{{user.Email}}</p>
               <p class="Profile">{{user.Diet}}</p>
               <p class="Profile">{{user.Password}}</p>
-              <!--Favoriten müssen auch aufgelistet werden-->
               
               
             </div>
@@ -36,7 +36,116 @@
       </div>
       </div>
       <br>
-    </div>
+    </div> -->
+
+
+
+
+
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with Dorang landing page.">
+    <meta name="author" content="Devcrud">
+    <title>Profil</title>
+
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home" class="dark-theme">
+
+
+    <div class="theme-selector">
+        <a href="javascript:void(0)" class="spinner">
+            <i class="ti-paint-bucket"></i>
+        </a>
+        <div class="body">
+            <a href="javascript:void(0)" class="light"></a>
+            <a href="javascript:void(0)" class="dark"></a>
+        </div>
+    </div>  
+
+    <br/><br/><br/><br/><br/><br/>
+    <!-- page header -->
+   
+
+    <!-- 1 -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+<div class="container bootstrap snippets bootdey">
+<div class="row">
+  <div class="profile-nav col-md-3" v-for="(user, key) in users" :key = 'key'>
+      <div class="panel">
+          <div class="user-heading round">
+              <a href="#">
+                  <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="">
+              </a>
+              <h1>{{user.Username}}</h1>
+              <p>{{user.Email}}</p>
+          </div>
+
+          <ul class="nav nav-pills nav-stacked">
+              <li><a href="editp.html"> <i class="fa fa-calendar"></i> Favorite<span class="label label-warning pull-right r-activity"></span> </a></li>
+              <li><a href="#"> <i class="fa fa-edit"></i> Edit profile</a></li>
+              
+          </ul>
+      </div>
+  </div>
+  <div class="profile-info col-md-9" v-for="(user, key) in users" :key = 'key'>
+     
+      <div class="panel">
+         
+          <div class="panel-body bio-graph-info">
+              <h1>User Profile</h1>
+              <div class="row">
+                  <div class="bio-row">
+                      <p><span>First Name: </span>Camila</p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Last Name: </span>Smith</p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Country: </span>Australia</p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Diet:</span>{{user.Diet}}</p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Email:</span>jsmith@mahidol.com</p>
+                  </div>
+                  <div class="bio-row">
+                      <p><span>Mobile:</span>(12) 03 4567890</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div>
+       
+      </div>
+  </div>
+</div>
+</div>
+
+<footer class="footer">
+                <p class="infos">&copy;, Made with <i class="ti-heart text-danger"></i> by Celina, Magnus, Till and Kevin</p>       
+                <span>|</span>  
+                <div class="links">
+                    <a href="#">About</a>
+                    <a href="#">Recipe</a>
+                    <a href="#">Profile</a>
+                    <button @click="logout" class="btn btn-default" style="color: red; border: solid">Logout</button>
+                </div>
+
+                    
+             
+            </footer>
+
+</body>
+</html>
+
+
+
+
+
+
+
   </template>
   
   <script>
@@ -78,6 +187,7 @@
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
   <style scoped>
+  @import '../assets/css/profile.css';
   h1, h2 {
     font-weight: normal;
   }
